78abcf44a57375f811b65ea57fadee59e8fad4a8
drygadmitrii-echo <drygadmitrii@gmail.com>
2025-08-27 21:29:32 +0300

    Обновление

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,1 +1,80 @@
-обновление
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from helpers.data import Urls
+import allure
+
+
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.base_url = Urls.MAIN_PAGE
+
+    def find_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(
+            EC.presence_of_element_located(locator),
+            message=f"Can't find element by locator {locator}"
+        )
+
+    def find_elements(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(
+            EC.presence_of_all_elements_located(locator),
+            message=f"Can't find elements by locator {locator}"
+        )
+
+    def find_clickable_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(
+            EC.element_to_be_clickable(locator),
+            message=f"Element not clickable: {locator}"
+        )
+
+    def go_to_site(self):
+        return self.driver.get(self.base_url)
+
+    @allure.step("Кликнуть на элемент")
+    def click_element(self, locator):
+        element = self.find_clickable_element(locator)
+        element.click()
+
+    @allure.step("Ввести текст: {text}")
+    def input_text(self, locator, text):
+        element = self.find_element(locator)
+        element.clear()
+        element.send_keys(text)
+
+    @allure.step("Получить текст элемента")
+    def get_text(self, locator):
+        element = self.find_element(locator)
+        return element.text
+
+    @allure.step("Дождаться видимости элемента")
+    def wait_for_element(self, locator, time=10):
+        return WebDriverWait(self.driver, time).until(
+            EC.visibility_of_element_located(locator)
+        )
+
+    @allure.step("Переключиться на новое окно")
+    def switch_to_new_window(self):
+        return self.driver.switch_to.window(self.driver.window_handles[1])
+
+    @allure.step("Принять куки")
+    def accept_cookies(self):
+        cookie_button = (By.ID, "rcc-confirm-button")
+        try:
+            self.click_element(cookie_button)
+        except:
+            pass
+
+    @allure.step("Скроллить к элементу")
+    def scroll_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
+
+    @allure.step("Кликнуть через JavaScript")
+    def click_via_js(self, element):
+        self.driver.execute_script("arguments[0].click();", element)
+
+    @allure.step("Ожидание загрузки страницы")
+    def wait_for_page_load(self, timeout=10):
+        WebDriverWait(self.driver, timeout).until(
+            lambda driver: driver.execute_script("return document.readyState") == "complete"
+        )
--- pages/order_page.py
+++ pages/order_page.py
@@ -1,1 +1,86 @@
-обновление
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
+import allure
+import time
+
+
+class OrderPageLocators:
+    # Форма заказа
+    NAME_INPUT = (By.XPATH, "//input[@placeholder='* Имя']")
+    LAST_NAME_INPUT = (By.XPATH, "//input[@placeholder='* Фамилия']")
+    ADDRESS_INPUT = (By.XPATH, "//input[@placeholder='* Адрес: куда привезти заказ']")
+    METRO_STATION_INPUT = (By.XPATH, "//input[@placeholder='* Станция метро']")
+    METRO_STATION_OPTION = (By.XPATH, "//div[@class='select-search__select']//li/button")
+    PHONE_INPUT = (By.XPATH, "//input[@placeholder='* Телефон: на него позвонит курьер']")
+    NEXT_BUTTON = (By.XPATH, "//button[text()='Далее']")
+
+    # Форма о аренде
+    DATE_INPUT = (By.XPATH, "//input[@placeholder='* Когда привезти самокат']")
+    RENTAL_PERIOD_DROPDOWN = (By.XPATH, "//div[text()='* Срок аренды']")
+    RENTAL_PERIOD_OPTION = (By.XPATH, "//div[@class='Dropdown-option']")
+    COLOR_CHECKBOX_BLACK = (By.ID, "black")
+    COLOR_CHECKBOX_GREY = (By.ID, "grey")
+    COMMENT_INPUT = (By.XPATH, "//input[@placeholder='Комментарий для курьера']")
+    ORDER_BUTTON = (By.XPATH, "//button[text()='Заказать' and contains(@class, 'Button_Middle__1CSJM')]")
+
+    # Модальное окно подтверждения
+    CONFIRM_BUTTON = (By.XPATH, "//button[text()='Да']")
+    SUCCESS_MESSAGE = (By.XPATH, "//div[contains(@class, 'Order_ModalHeader')]")
+
+
+class OrderPage(BasePage):
+    @allure.step("Заполнить первую страницу формы заказа")
+    def fill_first_page(self, name, last_name, address, metro_station, phone):
+        self.input_text(OrderPageLocators.NAME_INPUT, name)
+        self.input_text(OrderPageLocators.LAST_NAME_INPUT, last_name)
+        self.input_text(OrderPageLocators.ADDRESS_INPUT, address)
+
+        # Выбор станции метро
+        self.click_element(OrderPageLocators.METRO_STATION_INPUT)
+        metro_options = self.find_elements(OrderPageLocators.METRO_STATION_OPTION)
+        self.click_via_js(metro_options[metro_station])
+
+        self.input_text(OrderPageLocators.PHONE_INPUT, phone)
+        self.click_element(OrderPageLocators.NEXT_BUTTON)
+        time.sleep(2)  # Ожидание перехода на вторую страницу
+
+    @allure.step("Заполнить вторую страницу формы заказа")
+    def fill_second_page(self, date, rental_period, color, comment):
+        # Ждем загрузки второй страницы
+        time.sleep(2)
+
+        # Заполнение даты - используем правильный локатор
+        date_input = self.find_element(OrderPageLocators.DATE_INPUT)
+        date_input.clear()
+        date_input.send_keys(date)
+
+        # Нажимаем Enter чтобы подтвердить дату и закрыть календарь
+        date_input.send_keys("\n")
+
+        # Выбор срока аренды
+        self.click_element(OrderPageLocators.RENTAL_PERIOD_DROPDOWN)
+        period_options = self.find_elements(OrderPageLocators.RENTAL_PERIOD_OPTION)
+        self.click_via_js(period_options[rental_period])
+
+        # Выбор цвета
+        if color == "black":
+            checkbox = self.find_element(OrderPageLocators.COLOR_CHECKBOX_BLACK)
+            self.click_via_js(checkbox)
+
+        # Комментарий
+        self.input_text(OrderPageLocators.COMMENT_INPUT, comment)
+
+        # Скролл и клик на заказ
+        order_button = self.find_element(OrderPageLocators.ORDER_BUTTON)
+        self.scroll_to_element(order_button)
+        self.click_via_js(order_button)
+        time.sleep(1)  # Ожидание модального окна
+
+    @allure.step("Подтвердить заказ")
+    def confirm_order(self):
+        self.click_element(OrderPageLocators.CONFIRM_BUTTON)
+        time.sleep(1)  # Ожидание подтверждения
+
+    @allure.step("Получить текст сообщения об успешном заказе")
+    def get_success_message(self):
+        return self.get_text(OrderPageLocators.SUCCESS_MESSAGE)
